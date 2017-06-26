9d723602c65c2624091ba12f69c8f0ffe4ee7df1
Francisco San Emeterio <FSE@franciscos-mini.home>
2017-06-26 22:11:43 +0200

    Initial Commit

---
+++ snapshare/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  snapshare
+//
+//  Created by Francisco San Emeterio on 6/26/17.
+//  Copyright © 2017 Vinfranryia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
